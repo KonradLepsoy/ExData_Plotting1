5f5d076c8bc17ac0eed2a67ab1524984e77321d9
Lars Konrad Andreassen <konrad.lepsoy@gmail.com>
2015-04-10 12:46:36 +0200

    Final plot1 and plot2
    in addition to adding plot2 I changed to using the function require to
    load my sqldf package

--- plot2.R
+++ plot2.R
@@ -8,11 +8,11 @@ library(sqldf)
 # Get file from file location
 url<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 
-# Set working directory to Assignment 1-1 folder
-if (!file.exists("Assignment 1-1")) {
-  dir.create("Assignment 1-1")
+# Set working directory to Assignment 1 folder
+if (!file.exists("Assignment 1-2")) {
+  dir.create("Assignment 1-2")
 }
-setwd("./Assignment 1-1")
+setwd("./Assignment 1-2")
 
 # unzip file
 if (!file.exists("household_power_consumption.txt")) {
@@ -27,12 +27,17 @@ attr(wp, "file.format") <- list(sep = ";", header = TRUE)
 # use sqldf to read it in keeping only rows between '1/2/2007' and '2/2/2007'
 PowerConsumption <- sqldf("select * from wp where Date = '1/2/2007' or Date = '2/2/2007' ") 
 
-hist(PowerConsumption$Global_active_power, col="red",
-     xlab="Global Active Power (kilowatts)", ylab="Frequency",
-     main="Global Active Power")
+# Generate datetime adn convert it to date 
+PowerConsumption$DateTime <- as.POSIXlt( paste(PowerConsumption$Date, PowerConsumption$Time), format="%d/%m/%Y %H:%M")
 
-# Save the plot to a .png file
+# Create numeric
+PowerConsumption$globalActivePower <- as.numeric(PowerConsumption$globalActivePower)
+
+plot (PowerConsumption$DateTime, PowerConsumption$Global_active_power, type="l", 
+      xlab= "", ylab="Global active power (kilowatts)")
+
+# Save the plot to a .png file 
 # the default size for a .png is 480 x 480 pixels
-dev.copy(png, file="plot1.png")
+dev.copy(png, file="plot2.png", bg="transparent")
 dev.off()
 setwd("..") # go back to original working directory
